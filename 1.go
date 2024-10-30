751a01a7e5949f0a137aa5f4a59ee5407bb2371b
cqhasy <3181940782@qq.com>
2024-10-31 07:23:36 +0800

    Add files via upload

--- 1.go
+++ 1.go
@@ -2,29 +2,15 @@ package main
 
 import "fmt"
 
-var a, b, i, v int
-
-func mubiao(n []int, target int) []int {
-	for i = 0; i < v; i++ {
-		b = target - n[i]
-		for j := i + 1; j < v; {
-			if b == n[j] {
-				return []int{i, j}
-			}
-			j++
-
-		}
+func jishu() func() int {
+	var i int = 0
+	return func() int {
+		i++
+		return i
 	}
-	return nil
 }
 func main() {
-	var t []int
-	fmt.Scanf("%d%d", &a, &v)
-	for i := 0; i < v; i++ {
-		var l int
-		fmt.Scanf("%d", &l)
-		t = append(t, l)
-	}
-	fmt.Println(mubiao(t, a))
+	a := jishu()
+	fmt.Println(a())
+	fmt.Println(a())
 }
-
